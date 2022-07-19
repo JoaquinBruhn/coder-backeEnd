47c92f5fe0bb6a0a10f03f56bbb1dafeb3ac07e6
Joaquin Bruhn <joaquin_bruhn@hotmail.com.ar>
2022-07-19 19:48:37 -0300

    Class 8 challenge completed

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const app = express();
-
 const productosRouter = require("./routes/router");
-
-app.use(express.static("public"));
-
 const PORT = 8080;
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", productosRouter);
