6491a9b69a5de77539cff01758b87da2fb6aa6d6
Joaquin Bruhn <joaquin_bruhn@hotmail.com.ar>
2022-09-07 20:42:14 -0300

    Faker.js exercise done

--- routes/router.js
+++ routes/router.js
@@ -2,10 +2,12 @@ const express = require("express");
 const { Router } = express;
 const router = new Router();
 const productosRouter = require("./products/routeProducts");
+const productosTest = require("./products/routeProducts-test");
 const cartRouter = require("./cart/routeCart");
 const notFoundError = require("./errors/404");
 
 router.use("/api/productos", productosRouter);
+router.use("/api/productos-test", productosTest);
 router.use("/api/carrito", cartRouter);
 router.use("*", notFoundError);
 
