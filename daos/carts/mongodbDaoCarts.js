243e1e677c1c9e0fc9d9f915cd6a4ff874d2c133
Joaquin Bruhn <joaquin_bruhn@hotmail.com.ar>
2022-09-11 18:17:59 -0300

    Testing fixes to the removeProduct method on MongoDaoCarts

--- daos/carts/mongodbDaoCarts.js
+++ daos/carts/mongodbDaoCarts.js
@@ -33,18 +33,43 @@ class MongodbDaoCarts extends MongodbContainer {
     try {
       const productSearcher = new MongodbContainer(Product);
       const prodToAdd = await productSearcher.getById(prodId);
-      await this.schema.findByIdAndUpdate(cartId, { $push: { products: prodToAdd } });
-      const cartSearcher = new MongodbContainer(Cart);
-      const updatedCart = await cartSearcher.getById(cartId);
-      return updatedCart;
+      if (prodToAdd) {
+        await this.schema.findByIdAndUpdate(cartId, { $push: { products: prodToAdd } });
+        const cartSearcher = new MongodbContainer(Cart);
+        const updatedCart = await cartSearcher.getById(cartId);
+        return updatedCart;
+      } else {
+        throw new Object({ error: "Product does not exist" });
+      }
     } catch (error) {
       console.log(error);
     }
   }
+  async testeando(cartId, prodId) {
+    const currentCart = await this.schema.findById(cartId);
+    // delete currentCart.products[prodId];
+
+    // const updatedCart = await this.schema.updateOne(
+    //   { id: cartId },
+    //   {
+    //     $set: currentCart,
+    //   }
+    // );
+    console.log(currentCart);
+    const removedProd = currentCart.products.filter((el) => el._id !== prodId);
+    console.log(removedProd);
+
+    const cartUpdate = {
+      timestamp: timestamp(),
+      products: removedProd,
+    };
+
+    return cartUpdate;
+  }
 
   async removeProduct(cartId, prodId) {
     try {
-      await this.schema.findByIdAndUpdate(cartId, { $pull: { products: { _id: prodId } } });
+      await this.schema.findByIdAndUpdate(cartId, { $pull: { products: prodId } });
       const cartSearcher = new MongodbContainer(Cart);
       const updatedCart = await cartSearcher.getById(cartId);
       return updatedCart;
