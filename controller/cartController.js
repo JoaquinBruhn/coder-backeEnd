7ff4a09819cb17f4bba4dd48efcca9f1318f957d
Joaquin Bruhn <joaquin_bruhn@hotmail.com.ar>
2022-11-18 19:38:39 -0300

    finished the layering for the carts route

--- controller/cartController.js
+++ controller/cartController.js
@@ -1,33 +1,31 @@
 const cartService = require("../services/cartServices.js");
 const { cartsDB } = require("../daos/index.js");
 const { loggerApiError } = require("../middlewares/log4js/class32.js");
+const CartServices = require("../services/cartServices.js");
 
 class CartController {
-  static async getAllInCart(req, res) {
-    try {
-      const cart = await cartsDB.getAllInCart(req.params.id);
-      let totalPrice = 0;
-      cart.products.map((el) => {
-        totalPrice += el.price;
-      });
-      res.render("pages/cart", { cart: cart, totalPrice });
-    } catch (error) {
-      res.json("error, product not added");
-    }
-  }
-
   static async createCartNouser(req, res) {
     try {
-      const cartID = await cartsDB.createCart();
+      const cartID = await CartServices.createCart();
       res.json({ cartID });
     } catch (error) {
       loggerApiError.error("there has been an error", "n/", error);
     }
   }
 
+  static async getAllInCart(req, res) {
+    try {
+      const result = await CartServices.getAllInCart(req.params.id);
+      res.render("pages/cart", { cart: result.products, totalPrice: result.totalPrice, cartId: req.params.id });
+    } catch (error) {
+      loggerApiError.error("there has been an error", "n/", error);
+      res.json("error, cart not avaliable.");
+    }
+  }
+
   static async makePurchase(req, res) {
     try {
-      const result = await cartService.makePurchase(req.user);
+      const result = await CartServices.makePurchase(req.user);
       res.json(result);
     } catch (error) {
       loggerApiError.error("there has been an error", "n/", error);
@@ -36,17 +34,17 @@ class CartController {
 
   static async addProductToCart(req, res) {
     try {
-      const prodToAdd = await cartService.addProduct(req.user._id, req.body.id);
+      const prodToAdd = await CartServices.addProduct(req.user._id, req.body.id);
       res.json(`Success, ${prodToAdd.title} added to the cart.`);
     } catch (error) {
       res.json("error, product not added");
     }
   }
 
-  static async deleteAllCarts(req, res) {
+  static async removeProductFromCart(req, res) {
     try {
-      const cart = await cartsDB.deleteAll();
-      res.json(cart);
+      const result = await CartServices.removeProductFromCart(req.params.id, req.params.id_prod);
+      res.json(result);
     } catch (error) {
       loggerApiError.error("there has been an error", "n/", error);
     }
@@ -54,16 +52,16 @@ class CartController {
 
   static async deleteCartById(req, res) {
     try {
-      const cart = await cartsDB.deleteById(req.params.id);
+      const cart = await CartServices.deleteCartById(req.params.id);
       res.json(cart);
     } catch (error) {
       loggerApiError.error("there has been an error", "n/", error);
     }
   }
 
-  static async removeProductFromCart(req, res) {
+  static async deleteAllCarts(req, res) {
     try {
-      const result = await cartsDB.removeProduct(req.params.id, req.params.id_prod);
+      const result = await CartServices.deleteAllCarts();
       res.json(result);
     } catch (error) {
       loggerApiError.error("there has been an error", "n/", error);
--- services/cartServices.js
+++ services/cartServices.js
@@ -4,6 +4,28 @@ const Comunications = require("./comunications.js");
 const { loggerApiError } = require("../middlewares/log4js/class32.js");
 
 class CartServices {
+  static async createCartNouser() {
+    try {
+      const cartID = await cartsDB.createCart();
+      return cartID;
+    } catch (error) {
+      loggerApiError.error("there has been an error", "n/", error);
+    }
+  }
+
+  static async getAllInCart(cartID) {
+    try {
+      const products = await cartsDB.getAllInCart(cartID);
+      let totalPrice = 0;
+      products.products.map((el) => {
+        totalPrice += el.price;
+      });
+      return { products, totalPrice };
+    } catch (error) {
+      loggerApiError.error("there has been an error", "n/", error);
+    }
+  }
+
   static async addProduct(userId, prodId) {
     try {
       const userData = await mongodbDaoUsers.getUserData(userId);
@@ -14,6 +36,11 @@ class CartServices {
     }
   }
 
+  static async removeProductFromCart(cartId, prodId) {
+    const result = await cartsDB.removeProduct(cartId, prodId);
+    return result;
+  }
+
   static async makePurchase(userData) {
     try {
       const purchase = await cartsDB.makePurchase(userData.cart);
@@ -22,6 +49,16 @@ class CartServices {
     } catch (error) {
       loggerApiError.error("there has been an error", "n/", error);
     }
+  }
+
+  static async deleteCartById(cartId) {
+    const cart = await cartsDB.deleteById(cartId);
+    return cart;
+  }
+
+  static async deleteAllCarts() {
+    const cart = await cartsDB.deleteAll();
+    return cart;
   }
 }
 
